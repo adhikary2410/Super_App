a013df5ac4d6a0e9719ec2f4c5b8e537a14d4708
Subham Adhikary <subhamadhikary1020@gmail.com>
2024-08-17 23:49:25 +0530

    Part 1 changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import {BrowserRouter , Routes, Route} from 'react-router-dom'
+import RegisterPage from './pages/RegisterPage'
+import NotFoundPage from './pages/NotFoundPage'
 
 export default function App() {
   return (
     <div>
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RegisterPage/>}/>
+          <Route path="/register" element={<RegisterPage/>}/>
+          <Route path="/home" element={<RegisterPage/>}/>
+          <Route path="/genres" element={<RegisterPage/>}/>
+          <Route path="/carousel" element={<RegisterPage/>}/>
+          <Route path="/dashboard" element={<RegisterPage/>}/>
+          <Route path="/movies" element={<RegisterPage/>}/>
+          <Route path="*" element={<NotFoundPage/>}/> {/*any path not taken from above */}
+
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/RegisterPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './RegisterPage.module.css'
+
+export default function RegisterPage() {
+  return (
+    <div className={styles.container}>
+      <h1>Hello World</h1>
+    </div>
+  )
+}
