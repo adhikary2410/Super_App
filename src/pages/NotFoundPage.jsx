d227badb22e35229fc6dc0c95edbaeeeb8faebee
Subham Adhikary <subhamadhikary1020@gmail.com>
2024-08-19 02:46:37 +0530

    Lost Page

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './NotFoundPage.module.css'
+import { Link } from 'react-router-dom'
 
 export default function NotFoundPage() {
   return (
     <div className={styles.container}>
-      <h1>404 Not Found</h1>
-      <p>Page Doesn't Exist</p>
+      <h1 className={styles.heading}>404</h1>
+      <h3 className={styles.title}>Oops! Page Not Found</h3>
+      <p className={styles.desc}>The page you are looking for might have been removed, had its name changed, or is temporarily unavailable</p>
+      <Link className={styles.link} to="/">Go to Homepage</Link>
     </div>
   )
 }
